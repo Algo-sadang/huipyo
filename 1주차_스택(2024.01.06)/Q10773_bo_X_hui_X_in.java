87fa2653db098fbbf4d6bc5eeb7c4034d2624642
hhp227study <83169005+hhp227study@users.noreply.github.com>
2024-01-10 02:15:57 +0900

    Create Q10773_bo_X_hui_X_in.java

---
+++ 1주차_스택(2024.01.06)/Q10773_bo_X_hui_X_in.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        Stack<Integer> st = new Stack<>();
+        int ans = 0;
+
+        for(int i = 0; i < n; i ++){
+            int t = sc.nextInt();
+
+            if(t == 0){
+                ans -= st.pop();
+            } else{
+                st.push(t);
+                ans += t;
+            }
+        }
+        
+        System.out.println(ans);
+        sc.close();
+    }
+}
